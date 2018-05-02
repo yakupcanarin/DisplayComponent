036ea6f80f14289c0fdb46d638abb822dcd3efb5
Yakup (Jacob) Can Arın <ykpcn.arn@hotmail.com>
2018-05-02 15:43:04 +0300

    Webcam Component
    Working with webcam components

---
+++ Video/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Video
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.sbGrayScale = new System.Windows.Forms.HScrollBar();
+            this.sbColorRemap = new System.Windows.Forms.HScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sbContrast = new System.Windows.Forms.HScrollBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button2.Location = new System.Drawing.Point(167, 532);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Close Cam";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(12, 532);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Open Cam";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 497);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(875, 21);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(429, 479);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox2.Location = new System.Drawing.Point(475, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(411, 479);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button5.Location = new System.Drawing.Point(324, 532);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(119, 23);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Take a Picture";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button6.Location = new System.Drawing.Point(324, 561);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(119, 23);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "Save the Picture";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button7.Location = new System.Drawing.Point(12, 561);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(119, 23);
+            this.button7.TabIndex = 19;
+            this.button7.Text = "Clear PictureBox";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // sbGrayScale
+            // 
+            this.sbGrayScale.LargeChange = 100;
+            this.sbGrayScale.Location = new System.Drawing.Point(555, 581);
+            this.sbGrayScale.Maximum = 199;
+            this.sbGrayScale.Minimum = -100;
+            this.sbGrayScale.Name = "sbGrayScale";
+            this.sbGrayScale.Size = new System.Drawing.Size(331, 16);
+            this.sbGrayScale.SmallChange = 0;
+            this.sbGrayScale.TabIndex = 0;
+            this.sbGrayScale.ValueChanged += new System.EventHandler(this.sbGrayScale_ValueChanged);
+            // 
+            // sbColorRemap
+            // 
+            this.sbColorRemap.LargeChange = 0;
+            this.sbColorRemap.Location = new System.Drawing.Point(555, 615);
+            this.sbColorRemap.Minimum = -100;
+            this.sbColorRemap.Name = "sbColorRemap";
+            this.sbColorRemap.Size = new System.Drawing.Size(331, 15);
+            this.sbColorRemap.SmallChange = 0;
+            this.sbColorRemap.TabIndex = 22;
+            this.sbColorRemap.ValueChanged += new System.EventHandler(this.sbColorRemap_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(500, 537);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Contrast :";
+            // 
+            // sbContrast
+            // 
+            this.sbContrast.LargeChange = 0;
+            this.sbContrast.Location = new System.Drawing.Point(555, 537);
+            this.sbContrast.Maximum = 50;
+            this.sbContrast.Minimum = -50;
+            this.sbContrast.Name = "sbContrast";
+            this.sbContrast.Size = new System.Drawing.Size(331, 18);
+            this.sbContrast.SmallChange = 0;
+            this.sbContrast.TabIndex = 20;
+            this.sbContrast.ValueChanged += new System.EventHandler(this.sbContrast_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(487, 581);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Gray Scale :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(478, 617);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Color Remap :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 657);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sbColorRemap);
+            this.Controls.Add(this.sbGrayScale);
+            this.Controls.Add(this.sbContrast);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.HScrollBar sbGrayScale;
+        private System.Windows.Forms.HScrollBar sbColorRemap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.HScrollBar sbContrast;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Video/Form1.cs
@@ -0,0 +1,163 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using Accord.Video.FFMPEG;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using Accord.Imaging.Filters;
+using Accord.Imaging.Textures;
+using Accord;
+
+namespace Video
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private SaveFileDialog saveAvi;
+        private FilterInfoCollection VideoCaptureDevices;
+        private VideoCaptureDevice FinalVideo;
+        private VideoCaptureDeviceForm captureDevice = new VideoCaptureDeviceForm();
+       // private VideoFileWriter writer = new VideoFileWriter();
+        private string butStop = "";
+        private static Bitmap image;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VideoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in VideoCaptureDevices)
+            {
+                comboBox1.Items.Add(device);
+            }
+            captureDevice = new VideoCaptureDeviceForm();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FinalVideo = new VideoCaptureDevice(VideoCaptureDevices[comboBox1.SelectedIndex].MonikerString);
+            FinalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
+            FinalVideo.Start();
+        }
+
+        private void FinalVideo_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            if (butStop == "Stop Record")
+            {
+                image = (Bitmap)eventArgs.Frame.Clone();
+                pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+
+                //FileWriter.WriteVideoFrame(image);
+                //AVIwriter.AddFrame(video);
+            }
+            else
+            {
+                image = (Bitmap)eventArgs.Frame.Clone();
+                pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (FinalVideo.IsRunning == true)
+            {
+                FinalVideo.Stop();
+                pictureBox1.Image = null;
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+                Application.Exit(); 
+        }
+
+       
+        private void button5_Click(object sender, EventArgs e)
+        {
+            image = (Bitmap)pictureBox1.Image;
+            pictureBox2.Image = (Bitmap)pictureBox1.Image.Clone();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image != null)
+            {
+                saveAvi = new SaveFileDialog();
+                saveAvi.Filter = "Bitmap Image (.bmp)|*.bmp|Gif Image (.gif)|*.gif|JPEG Image (.jpeg)|*.jpeg|Png Image (.png)|*.png|Tiff Image (.tiff)|*.tiff|Wmf Image (.wmf)|*.wmf";
+                if (saveAvi.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox2.Image.Save(saveAvi.FileName);
+                    MessageBox.Show("The photo has saved.", "Success", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show("The photo couldn't save.", "Fail", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = null;
+            pictureBox2.Invalidate();
+        }
+
+        private void sbContrast_ValueChanged(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(image); 
+            ContrastCorrection filter = new ContrastCorrection(sbContrast.Value);
+            filter.ApplyInPlace(img);
+            pictureBox2.Image = img;
+        }
+
+        private void sbGrayScale_ValueChanged(object sender, EventArgs e)
+        {
+            
+            double cr = 0.00;
+            double cg = 0.00;
+            double cb = 0.00;
+            double value = sbGrayScale.Value / 100f;
+            cr = +(value * 0.2);
+            cg = +(value * 0.7);
+            cb = +(value * 0.7);
+           
+            Bitmap img = new Bitmap(image);
+            Grayscale filter = new Grayscale(cr,cg,cb);
+            Bitmap grayImage = filter.Apply(img);
+            if (value == 0)
+            {
+                pictureBox2.Image = image;
+            }
+            else
+            {
+                pictureBox2.Image = grayImage;
+            }
+
+        }
+
+        private void sbColorRemap_ValueChanged(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(image);
+            LevelsLinear filter = new LevelsLinear();
+            // set ranges
+            filter.InRed = new IntRange(sbColorRemap.Value, 300);
+            filter.InGreen = new IntRange(sbColorRemap.Value, 300);
+            filter.InBlue = new IntRange(sbColorRemap.Value, 300);
+            // apply the filter
+            filter.ApplyInPlace(img);
+            pictureBox2.Image = img;
+        }
+    }
+}
